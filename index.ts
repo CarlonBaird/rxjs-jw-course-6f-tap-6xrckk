9358816a816e87ebaf84d3eaf40d5b1bfb7fd728
CarlonBaird <carlyestrella@live.com>
2022-11-22 15:38:22 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -3,8 +3,11 @@ import { filter, map, tap } from 'rxjs/operators';
 
 of(1, 7, 3, 6, 2)
   .pipe(
-    tap((value) => console.log('Spy:', value)),
-    map((value) => value * 2),
-    filter((value) => value > 5)
+    filter((value) => value > 5), //filter first
+    // tap((value) => console.log('Spy:', value)), //take a peek at the values after filtering
+    tap({
+      next: (value) => console.log('Spy:', value),
+    }),
+    map((value) => value * 2) //lastly map
   )
   .subscribe((value) => console.log('Output:', value));
