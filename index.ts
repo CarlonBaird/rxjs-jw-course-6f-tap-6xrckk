3c614349191daa61eaf508c5306120ac0582d341
CarlonBaird <carlyestrella@live.com>
2022-11-22 15:14:04 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,3 +1,10 @@
 import { of } from 'rxjs';
+import { filter, map, tap } from 'rxjs/operators';
 
-of(1, 7, 3, 6, 2).subscribe((value) => console.log('Output:', value));
+of(1, 7, 3, 6, 2)
+  .pipe(
+    tap((value) => console.log('Spy:', value)),
+    map((value) => value * 2),
+    filter((value) => value > 5)
+  )
+  .subscribe((value) => console.log('Output:', value));
