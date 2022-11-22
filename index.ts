6ba1343d5ff935d7d3c4332058e91f751947ba3f
CarlonBaird <carlyestrella@live.com>
2022-11-22 15:40:15 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -4,9 +4,11 @@ import { filter, map, tap } from 'rxjs/operators';
 of(1, 7, 3, 6, 2)
   .pipe(
     filter((value) => value > 5), //filter first
+    //the 'tap' operator can be used to debug
     // tap((value) => console.log('Spy:', value)), //take a peek at the values after filtering
     tap({
       next: (value) => console.log('Spy:', value),
+      complete: () => console.log('Complete'),
     }),
     map((value) => value * 2) //lastly map
   )
